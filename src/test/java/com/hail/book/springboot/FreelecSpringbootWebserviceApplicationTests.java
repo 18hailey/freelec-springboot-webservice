b047b7a4259e3ba42e9f0a30d131fe8b749bc229
18hailey <hailey9459@gmail.com>
2024-09-29 17:11:43 +0900

    fixed untracked files

---
+++ src/test/java/com/hail/book/springboot/FreelecSpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hail.book.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
