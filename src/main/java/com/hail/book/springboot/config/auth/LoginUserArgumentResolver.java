a4c2f5c84758caef51507b86a1311493ea36ce00
18hailey <hailey9459@gmail.com>
2024-10-04 23:37:03 +0900

    Google Login 구현 - 세션 코드 애너테이션으로 처리

---
+++ src/main/java/com/hail/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,40 @@
+package com.hail.book.springboot.config.auth;
+
+import com.hail.book.springboot.config.auth.dto.SessionUser;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    /**
+     * HandlerMethodArgumentResolver는
+     * 조건에 맞는 경우 메서드가 있다면 HandlerMethodArgumentResolver의 구현체가 지정한 값으로
+     * 해당 메소드의 파라미터로 넘길 수 있다
+     */
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    // 파라미터에 전달할 객체 생성
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+
+}
